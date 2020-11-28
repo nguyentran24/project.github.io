0caf76af0f58151b6268474cdf189fcbf8aea775
nguyentran24 <75112833+nguyentran24@users.noreply.github.com>
2020-11-28 22:25:30 +0700

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,12 @@
+-- Database: project
+
+-- DROP DATABASE project;
+
+CREATE DATABASE project
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
